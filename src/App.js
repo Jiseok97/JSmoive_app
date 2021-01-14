f5a179e06c0ddd72ff7ad943bcb86ee0f84ea6cd
netLee <wltjr1426@naver.com>
2021-01-14 16:46:59 +0900

    Study react

--- src/App.js
+++ src/App.js
@@ -1,48 +1,56 @@
 import react from "react";
 
-// (props) -> ({ fav }) (= props.fav)
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
-
+// 각각의 id는 react 내부엥서 사용하기 위함
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.PUI3Pc7QE-YOjLxm-BwZPQHaFP%26pid%3DApi&f=1",
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
   },
 ];
 
+// (props) -> ({ fav }) (= props.fav)
+// alt는 시각장애인을 위한 react-app 에서 제공
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        // key는 위에 Food 함수로 가지 않고, react 내부에서 사용
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
