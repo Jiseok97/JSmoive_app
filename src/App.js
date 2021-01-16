5a069f2a7da7cc9e0756b1633ca385c69a7ce77e
netLee <wltjr1426@naver.com>
2021-01-17 01:56:56 +0900

    Use axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import react from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 // PropTypes를 통해 props 체크
 
 class App extends react.Component {
@@ -7,11 +8,15 @@ class App extends react.Component {
     isLoading: true,
     movies: [],
   };
+  getMoives = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+  };
   componentDidMount() {
-    setTimeout(() => {
-      // 6초 딜레이
-      this.setState({ isLoading: false });
-    }, 6000);
+    // setTimeout(() => {
+    //   // 6초 딜레이
+    //   this.setState({ isLoading: false });
+    // }, 6000);
+    this.getMoives();
   }
   render() {
     const { isLoading } = this.state; // isLoading을 호출할 때, 앞에 this.state 생략을 위함
