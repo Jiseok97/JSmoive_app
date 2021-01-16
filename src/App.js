98d9df20a8dc0aa8aefe217775c3a7c904010aeb
netLee <wltjr1426@naver.com>
2021-01-16 13:28:19 +0900

    Study state

--- src/App.js
+++ src/App.js
@@ -1,78 +1,23 @@
 import react from "react";
 import PropTypes from "prop-types";
-
-// 각각의 id는 react 내부엥서 사용하기 위함
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.PUI3Pc7QE-YOjLxm-BwZPQHaFP%26pid%3DApi&f=1",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.1,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 3.9,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.3,
-  },
-];
-
-// (props) -> ({ fav }) (= props.fav)
-// alt는 시각장애인을 위한 react-app 에서 제공
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating} / 5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
 // PropTypes를 통해 props 체크
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
 
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        // key는 위에 Food 함수로 가지 않고, react 내부에서 사용
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends react.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {};
+  minus = () => {};
+  render() {
+    // react.component는 retrun 을 사용 x
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
