50299327bda8364d026ede9f781ab1e96d980e26
netLee <wltjr1426@naver.com>
2021-01-22 20:44:58 +0900

    Move the movie-detail

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
+import "./App.css";
 
 // BrowserRouter을 HashRouter 대신 사용이 가능하나,
 // github pages에 정확히 설정하기가 까다롭다
@@ -15,6 +17,7 @@ function App() {
       <Route path="/" exact={true} component={Home} />{" "}
       {/* excat=ture는 "/"일 때만 route 시킴*/}
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
--- src/components/Moive.js
+++ src/components/Moive.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 // PropTypes를 통해 props 체크
@@ -6,22 +7,35 @@ import "./Movie.css";
 function Movie({ year, title, summary, poster, genres }) {
   return (
     // 각 요소 안에 style로 css에서 안하고 직접 css를 처리할 수 있음
-    <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        {/* array.slice -> 시작점부터 끝점까지만 보여지게 하기 */}
-        <p className="movie__summary">{summary.slice(0, 140)}...</p>
+    <Link
+      to={{
+        pathname: "/movie-detail",
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          {/* array.slice -> 시작점부터 끝점까지만 보여지게 하기 */}
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
